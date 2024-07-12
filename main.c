914fdfb2384038dd93b10bd91125189139eef06e
Jesse Williams <jesse@negativespacesounds.com>
2024-07-12 17:53:07 +1000

    update: add threading, ensuring stack allocations only

--- main.c
+++ main.c
@@ -18,12 +18,48 @@ typedef struct {
   struct sockaddr_in client_addr;
 } client_info_t;
 
+typedef struct {
+  int status_code;
+  size_t content_length;
+  char *content_type;
+  char *content;
+} http_response_t;
+
+void create_response(int status_code, char *content_type, char *content,
+                     http_response_t *responses, int thread_index) {
+  if (content == NULL) {
+    return;
+  }
+  size_t content_length = strlen(content);
+  responses[thread_index].content_length = content_length;
+  responses[thread_index].status_code = status_code;
+  responses[thread_index].content_type = content_type;
+  responses[thread_index].content = content;
+  return;
+}
+
+char *buffer_ok_response(http_response_t *response, char *buffer) {
+  sprintf(buffer,
+          "HTTP/1.1 %d OK\r\nContent-Length: %ld\r\nContent-Type: %s\r\n\r\n%s",
+          response->status_code, response->content_length,
+          response->content_type, response->content);
+  return buffer;
+}
+
+typedef struct {
+  client_info_t *client_info;
+  http_response_t *responses;
+  int thread_index;
+} handle_client_args_t;
+
 void *handle_client(void *arg) {
-  client_info_t *client_info = (client_info_t *)arg;
-  int connection_file_descriptor = client_info->connection_file_descriptor;
-  char receiver_buffer[BUFFER_SIZE] = "",
-       sender_buffer[BUFFER_SIZE] =
-           "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
+  handle_client_args_t *args = (handle_client_args_t *)arg;
+  int connection_file_descriptor =
+      args->client_info->connection_file_descriptor;
+  char receiver_buffer[BUFFER_SIZE] = "", sender_buffer[BUFFER_SIZE] = "";
+  create_response(200, "text/html", "Hello, World!", args->responses,
+                  args->thread_index);
+  buffer_ok_response(&args->responses[args->thread_index], sender_buffer);
   // receive messages from the client
   recv(connection_file_descriptor, receiver_buffer, sizeof receiver_buffer, 0);
   printf("\n[client] %s", receiver_buffer);
@@ -67,26 +103,29 @@ int main(void) {
     return 1;
   }
   init_server(server, socket_file_descriptor);
-  client_info_t client_info[MAX_THREADS];
-  pthread_t thread[MAX_THREADS];
+  handle_client_args_t args[MAX_THREADS];
+  client_info_t clients[MAX_THREADS];
+  http_response_t responses[MAX_THREADS];
+  pthread_t threads[MAX_THREADS];
   int thread_count = 0;
-
   while (1) {
-    n = sizeof client_info[thread_count].client_addr;
-    client_info[thread_count].connection_file_descriptor =
+    n = sizeof clients[thread_count].client_addr;
+    clients[thread_count].connection_file_descriptor =
         accept(socket_file_descriptor, (struct sockaddr *)&client, &n);
-    if (client_info[thread_count].connection_file_descriptor == -1) {
-      perror("Error creating connection\n");
+    if (clients[thread_count].connection_file_descriptor == -1) {
+      perror("Error creating connection file descriptor\n");
       continue;
     }
-    if (pthread_create(&thread[thread_count], NULL, handle_client,
-                       &client_info[thread_count]) != 0) {
+    args[thread_count].client_info = &clients[thread_count];
+    args[thread_count].responses = responses;
+    args[thread_count].thread_index = thread_count;
+    if (pthread_create(&threads[thread_count], NULL, handle_client,
+                       &args[thread_count]) != 0) {
       perror("Error creating thread\n");
-      close(client_info[thread_count].connection_file_descriptor);
+      close(clients[thread_count].connection_file_descriptor);
       continue;
     }
-
-    pthread_detach(thread[thread_count]);
+    pthread_detach(threads[thread_count]);
     thread_count = (thread_count + 1) % MAX_THREADS;
   }
   close(socket_file_descriptor);
